402a7f15cbbec9a3a335cdba8967fba9e7846c02
Igor Kremenskoy <gores.73@web.de>
2023-11-24 11:41:15 +0100

    add comments

--- src/Main.java
+++ src/Main.java
@@ -2,4 +2,14 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+    //    Создали удаленный репозиторий
+//    Создали локальный и вводим команды
+//    git init
+//    создаем .gitignore
+//    git remote add origin git@github.com:AlisherKhamidov/example_05.git
+//    git add .
+//    git commit -m 'init'
+//    git push origin main или git push и выбираем из предложенных вариантов
+//    git pull origin main - подтянуть
+
 }
